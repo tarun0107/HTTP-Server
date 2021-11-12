950933471f801fcf2428a0d76639e822a7bc5fd3
tarun0107 <tapadiyatr19.comp@coep.ac.in>
2021-11-13 00:15:22 +0530

    HEAD

--- httpserver.py
+++ httpserver.py
@@ -20,6 +20,7 @@ statuscodes = {
     404: 'Not Found',
     408: 'Request Timeout',
     411: 'Length Required',
+    412: 'Precondition Failed',
     413: 'Payload Too Large',
     414: 'URI Too Long',
     415: 'Unsupported Media Type',
@@ -38,7 +39,7 @@ extentions = {'txt': 'text/plain',
 Headers = {
     'Date' : 'Time',
     'Server' : 'Apache/2.4.18 (Ubuntu)',
-    'Last-Modified' : 'Mon, 18 Aug 2020 12∶51∶23 GMT',
+    'Last-Modified' : 'Mon, 19 Aug 2021 12∶51∶23 GMT',
     'Connection' : 'Closed',
     'Accept-Ranges' : 'bytes',
     'Content-Length' : '10000',
@@ -48,12 +49,14 @@ Headers = {
 Months = {'Jan' : 1, 'Feb' : 2, 'Mar' : 3, 'Apr' : 4, 'May' : 5, 'Jun' : 6, 
 'Jul' : 7, 'Aug' : 8, 'Sep' : 9, 'Oct' : 10, 'Nov' : 11, 'Dec' : 12}
 
+versions = {"1.0", "1.1"}
+
 def write_log(stcode, time1, url, file):
     time1 = time1.strftime("%d/%b/%Y:%H:%M:%S")
     method = url[0:6] 
     if(stcode == 200 and method == 'DELETE'):
         log = "127.0.0.1 - - " + "[" + str(time1) + " +0530] "  + '"' + url + '" ' + str(stcode) + " 0" + ' "-" "-"\r\n' 
-    elif(stcode == 400 or stcode == 415 or stcode == 404 or stcode == 414 or stcode == 403 or stcode == 401 or stcode == 411 or stcode == 413):
+    elif(stcode == 400 or stcode == 415 or stcode == 404 or stcode == 414 or stcode == 403 or stcode == 401 or stcode == 411 or stcode == 413 or stcode == 501):
         log = "127.0.0.1 - - " + "[" + str(time1) + " +0530] "  + '"' + url + '" ' + str(stcode) + " 0" + ' "-" "-"\r\n' 
     elif(stcode == 200 or stcode == 201 or stcode == 204 or stcode == 304 or stcode == 202):
         log = "127.0.0.1 - - " + "[" + str(time1) + " +0530] "  + '"' + url + '" ' + str(stcode) + " " + str(os.stat(file).st_size) + ' "-" "-"\r\n' 
@@ -103,7 +106,7 @@ def if_modified_since(date, file):
                 if new[4] > date[4]:
                     flag = 1
     return flag
-
+    
 def user_auth(data):
     h = data[0].split(' : ')
     headers = h[1].split(' ')[1]
@@ -117,6 +120,14 @@ def user_auth(data):
     except(binascii.Error):
         return False
     return False
+def HTTP_400():
+    stcode = 400
+    response_body = "<html>\n<head>\n<title>400 Bad Request</title>\n</head>\n<body>\n<p>Server could not understand the request you sent.</p>\n<hr>\n<address>Apache/2/4/18 (Ubuntu) Server at 127.0.0.1 </address>\n</body>\n</html>\n"
+    l = len(response_body)
+    date_time1 = datetime.datetime.now()
+    date_time = givedate(date_time1)
+    response = get_headers(400, date_time, l)
+    return response, response_body
 def HTTP_401():
     stcode = 401
     response_body = "<html>\n<head>\n<title>401 Unauthorized</title>\n</head>\n<body>\n<p>You are not allowed to make changes.</p>\n<hr>\n<address>Apache/2/4/18 (Ubuntu) Server at 127.0.0.1 </address>\n</body>\n</html>\n"
@@ -147,7 +158,15 @@ def HTTP_411():
     l = len(response_body)
     date_time1 = datetime.datetime.now()
     date_time = givedate(date_time1)
-    response = get_headers(411, l)
+    response = get_headers(411, date_time, l)
+    return response, response_body
+def HTTP_412():
+    stcode = 412
+    response_body = "<html>\n<head>\n<title>412 Precondition Failed</title>\n</head>\n<body>\n<h4>The requested process can't be processed</h4>\n</body><\html>"
+    l = len(response_body)
+    date_time1 = datetime.datetime.now()
+    date_time = givedate(date_time1)
+    response = get_headers(412, date_time, l)
     return response, response_body
 def HTTP_413():
     stcode = 413
@@ -155,7 +174,7 @@ def HTTP_413():
     l = len(response_body)
     date_time1 = datetime.datetime.now()
     date_time = givedate(date_time1)
-    response = get_headers(413, l)
+    response = get_headers(413, date_time, l)
     return response, response_body
 def HTTP_414():
     stcode = 414
@@ -163,7 +182,7 @@ def HTTP_414():
     l = len(response_body)
     date_time1 = datetime.datetime.now()
     date_time = givedate(date_time1)
-    response = get_headers(415, date_time, l)
+    response = get_headers(414, date_time, l)
     return response, response_body
 def HTTP_415():
     stcode = 415
@@ -173,7 +192,14 @@ def HTTP_415():
     date_time = givedate(date_time1)
     response = get_headers(415, date_time, l)
     return response, response_body
-
+def HTTP_501():
+    stcode = 501
+    response_body = "<html>\n<head>\n<title>501 Not Implemented</title>\n</head>\n</html>"
+    l = len(response_body)
+    date_time1 = datetime.datetime.now()
+    date_time = givedate(date_time1)
+    response = get_headers(501, date_time, l)
+    return response, response_body
 def GET(url, connectionsocket, addr, data):
     url_len = len(url)
     global stcode
@@ -183,7 +209,8 @@ def GET(url, connectionsocket, addr, data):
         stcode = 414
         return response, response_body
     headers = data.split('\r\n')
-    ims = [string for string in headers if "if-modified-since" in string]
+    ims = [string for string in headers if "If-Modified-Since" in string]
+    ius = [string for string in headers if "If-Unmodified-Since" in string]
     final_url = url.split('/')
     filename = final_url[-1]
     file = url
@@ -193,10 +220,15 @@ def GET(url, connectionsocket, addr, data):
         ifms = if_modified_since(date, file)
     else:
         ifms = 1
+    if len(ius) > 0:
+        date = ius[0].split(' : ')
+        date = date[-1]
+        ifus = if_modified_since(date, file)
+    else:
+        ifus = 1
     extension = final_url[-1].split('.')
     extension = extension[-1]
-    if(os.path.exists(file) and (os.access(file, os.W_OK) == False or os.access(file, os.R_OK) == False) and url != None):
-        
+    if(os.path.exists(file) and (os.access(file, os.W_OK) == False or os.access(file, os.R_OK) == False) and url != None): 
         stcode = 403
         response, response_body = HTTP_403()
     else:
@@ -217,6 +249,26 @@ def GET(url, connectionsocket, addr, data):
                 else:
                     stcode = 404
                     response, response_body = HTTP_404()
+            elif(ifus == 0):
+                if(os.path.exists(file)):
+                    date_time1 = datetime.datetime.now()
+                    date_time = givedate(date_time1) 
+                    stcode = 200
+                    if flag == 1:
+                        f = open(file, 'rb')
+                        response_body = f.read()
+                    else:
+                        f = open(file, 'r')
+                        response_body = f.read()
+                    l = len(response_body)
+                    response = get_headers(stcode, date_time, l, url, extension, file)
+                else:
+                    stcode = 404
+                    response, response_body = HTTP_404()
+            elif (ifus):
+                stcode = 412
+                response, response_body = HTTP_412()
+
             else:
                 if os.path.exists(file):
                     if flag == 1:
@@ -390,48 +442,52 @@ def PUT(url, connectionsocket, addr, data):
     if (os.path.exists(file) and (os.access(file, os.W_OK) == False or os.access(file, os.R_OK) == False) and url != None):
         stcode = 403
         response, response_body = HTTP_403()    
-    elif extention in extentions:
-        if cont_len:
-            if len(lines) < max_payload:
-                if os.path.exists(file):
-                    if auth:
-                        if user_auth(auth):
-                            f = open(file, "w")
+    else:
+        if extention in extentions:
+            if cont_len:
+                if len(lines) < max_payload:
+                    if os.path.exists(file):
+                        if auth:
+                            if user_auth(auth):
+                                f = open(file, "w")
+                                f.write(lines)
+                                stcode = 200
+                                response_body = lines
+                                date_time1 = datetime.datetime.now()
+                                date_time = givedate(date_time1)
+                                response = get_headers(stcode, date_time, len(lines), url, extention, file)
+                        
+                            else:
+                                stcode = 401
+                                response, response_body = HTTP_401()
+                        else:
+                            f = open(file, 'w')
                             f.write(lines)
                             stcode = 200
                             response_body = lines
                             date_time1 = datetime.datetime.now()
                             date_time = givedate(date_time1)
                             response = get_headers(stcode, date_time, len(lines), url, extention, file)
-                        
-                        else:
-                            stcode = 401
-                            response, response_body = HTTP_401()
                     else:
+                        f = open(file, 'x')
                         f = open(file, 'w')
                         f.write(lines)
-                        stcode = 200
+                        stcode= 201
                         response_body = lines
                         date_time1 = datetime.datetime.now()
                         date_time = givedate(date_time1)
                         response = get_headers(stcode, date_time, len(lines), url, extention, file)
-                else:
-                    f = open(file, 'x')
-                    f = open(file, 'w')
-                    f.write(lines)
-                    stcode= 201
-                    response_body = lines
-                    date_time1 = datetime.datetime.now()
-                    date_time = givedate(date_time1)
-                    response = get_headers(stcode, date_time, len(lines), url, extention, file)
                         
-            else:
-                stcode = 413
-                response, response_body = HTTP_413()
+                else:
+                    stcode = 413
+                    response, response_body = HTTP_413()
 
+            else:
+                stcode = 411
+                response, response_body = HTTP_411()
         else:
-            stcode = 411
-            response, response_body = HTTP_411()
+            stcode = 415
+            response, response_body = HTTP_415()  
     if stcode == 411 or stcode == 413 or stcode == 403:
         write_log(stcode, datetime.datetime.now(), url1, file)
     else:
@@ -439,7 +495,65 @@ def PUT(url, connectionsocket, addr, data):
         f = open('access.log', 'a')
         f.write(log)
     return response, response_body
-    
+
+def HEAD(url, connectionsocket, addr, data):
+    url_len = len(url)
+    global stcode 
+    if url_len > max_uri_len:
+        response, response_body = HTTP_414()
+        stcode = 414
+        return response
+    data = data.split('\r\n')
+    url1 = "HEAD " + url
+    file = url
+    extention = file.split('.')[-1]
+    headers = data.split('\r\n')
+    ims = [string for string in headers if "If-Modified-Since" in string]
+    ius = [string for string in headers if "If-Unmodified-Since" in string]
+    if len(ims) > 0:
+        date = ims[0].split(' : ')
+        date = date[-1]
+        ifms = if_modified_since(date, file)
+    else:
+        ifms = 1
+    if len(ius) > 0:
+        date = ius[0].split(' : ')
+        date = date[-1]
+        ifus = if_modified_since(date, file)
+    else:
+        ifus = 1
+    if (os.path.exists(file) and (os.access(file, os.W_OK) == False or os.access(file, os.R_OK) == False) and url != None):
+        stcode = 403
+        response, response_body = HTTP_403() 
+    else:
+        if extention in extentions:
+            try:
+                if extention in aud_vd:
+                    flag = 1
+            except(UnboundLocalError):#assigning value to local variable before declaring it
+                flag = 0
+            if os.path.exists(file):
+                if flag == 1:
+                    f = open(file, 'rb')
+                    response_body = f.read()
+                else:
+                    f = open(file, 'r')
+                    response_body = f.read()
+                l = len(response_body)
+                date_time1 = datetime.datetime.now()
+                date_time = givedate(date_time1)
+                if ifms == 0:
+                    stcode = 304
+                elif ifus == 1:
+                    stcode == 412
+                else:
+                    stcode = 200
+                response = get_headers(200, date_time,l, url, extention, file)
+            else:
+                stcode = 404
+                response, response_body = HTTP_404()
+    return response 
+
 def HTTPRequest(request):
     version = '1.1'
     request = request.split('\r\n')
@@ -447,7 +561,7 @@ def HTTPRequest(request):
     method = words[0]
     url = words[1]
     if len(words) > 2:
-        version = words[2]
+        version = words[2].split('/')[-1]
     return version, method, url
 def call_methods(connectionsocket, addr, data):
     global stcode 
@@ -455,6 +569,13 @@ def call_methods(connectionsocket, addr, data):
     fp = data.split('\r\n')
     time1 = datetime.datetime.now()
     version, method, url = HTTPRequest(data)
+    print(version)
+    if version not in versions:
+        stcode = 400
+        response, response_body = HTTP_400()
+        res = response + response_body
+        connectionsocket.sendall(res.encode())
+        write_log(stcode, datetime.datetime.now(), fp[0], url)
     if method == 'GET':
         response, response_body = GET(url, connectionsocket, addr, data)
         res = response + response_body
@@ -473,7 +594,17 @@ def call_methods(connectionsocket, addr, data):
         response, response_body = PUT(url, connectionsocket, addr, data)
         res = response + response_body
         connectionsocket.sendall(res.encode())
-def start_restart_stop(serversocket):
+    elif method == 'HEAD':
+        response = HEAD(url, connectionsocket, addr, data)
+        connectionsocket.sendall(response.encode())
+        write_log(stcode, datetime.datetime.now(), fp[0], url) 
+    else:
+        stcode = 501
+        response, response_body = HTTP_501()
+        connectionsocket.sendall((response+response_body).encode())
+        write_log(stcode, datetime.datetime.now(), fp[0], url)
+
+def start_restart_stop(serversocket, serverport, host):
     while True:
         action = input()
         action = action.lower()
@@ -482,18 +613,23 @@ def start_restart_stop(serversocket):
             print("Exiting...")
             os._exit(0)
             break
-
+        elif action == 'restart':
+            print("Restarting...")
+            time.sleep(5)
+            serversocket.bind(('127.0.0.1', serverport))
+            serversocket.listen(10)
+            print("Restarted...")
 serversocket = socket(AF_INET, SOCK_STREAM)
+serversocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 servername = '127.0.0.1'
 serverport = int(sys.argv[1])
+host = serversocket.getsockname()
 serversocket.bind(('', serverport))
 serversocket.listen(10)
+Thread1 = Thread(target=start_restart_stop, args=(serversocket, serverport, host))
+Thread1.start()
 while True:
     connectionsocket, addr = serversocket.accept()
     print("Connected By :", addr)
     data = (connectionsocket.recv(8192))
     call_methods(connectionsocket, addr, data)
- 
-
-
-  
